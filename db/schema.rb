037b6e0de0a44faa06f580fcf43783ea01a14aa2
andresilveira <andresilveirah@gmail.com>
2015-08-08 16:56:57 +0200

    add teacher

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806144634) do
+ActiveRecord::Schema.define(version: 20150808134729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150806144634) do
   create_table "rooms", force: :cascade do |t|
     t.string   "name"
     t.integer  "capacity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
